5b0eaafc43c12045222eae585a7239c94469ad5d
amrozeidan <48893895+amrozeidan@users.noreply.github.com>
2019-10-23 15:36:58 +0200

    Nico's additions on plots
    requiredStationsFile in arguments, to locate the required stations file
    rmse, mea and me are added in different way to the plots:
    -plot 1: subplot 1.1: measured wl and simulated wl
                 subplot 1.2: wl difference and moving average
    -plot 2: scatter_kde.m is used with measured wl and simulated wl
    -plot 3: subplot 3.1: rmse and mae for locations
                 subplot 3.2: me for locations

--- d_wlcomp.m
+++ d_wlcomp.m
@@ -20,7 +20,7 @@
 %    covering Jan and Feb
 % e) k moving average steps
 
-function d_wlcomp( common_folder , basefolder , period , k, offset)
+function d_wlcomp( common_folder , basefolder , period , k, offset, requiredStationsFile)
 
 % main_path = '/Users/amrozeidan/Desktop/EasyGSH/3_functions_rev16c_20190523';
 % addpath(main_path)
@@ -56,24 +56,29 @@ Ttsimul.TimeStep_No = datetime (Ttsimul.TimeStep_No , 'InputFormat' , 'dd/MM/yyy
 Ttsimul = table2timetable(Ttsimul);
 
 %importing required station names
-filepath_req = strcat(common_folder, '/required_stations.dat');
-req_data = textread( filepath_req , '%s', 'delimiter', '\n')';
+%requiredStationsFile = strcat(common_folder, '/required_stations.dat');
+req_data = textread( requiredStationsFile , '%s', 'delimiter', '\n')';
 
 %intersection of required stations, simulations and measurements:
-stations = intersect(intersect(Ttmeas.Properties.VariableNames , Ttsimul.Properties.VariableNames) , req_data );
+stations = intersect(Ttsimul.Properties.VariableNames , Ttmeas.Properties.VariableNames);
+stations = intersect(req_data, stations);
+
+stations = req_data(ismember(req_data, stations));
+
 
 %%
 
 data_nrmse = [];
 data_rmse = [];
+data_me = [];
 data_mae = [];
 
 %creating folders for results storage
 mkdir(basefolder, 'wl_comparison')
 path_3 = strcat(basefolder, '/wl_comparison') ;
 
-path_4 = strcat(common_folder , '/measurements') ;
-path_5 = strcat(basefolder , '/telemac_variables/variables_all_stations') ;
+%path_4 = strcat(common_folder , '/measurements') ;
+%path_5 = strcat(basefolder , '/telemac_variables/variables_all_stations') ;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % %updating timetables for all stations
@@ -100,7 +105,7 @@ for cwl=1:length(stations)
     
     %reading simulated data
     simul_dates = Ttsimul.TimeStep_No;
-    %simul_dates = simul_dates + hours(offset);
+    simul_dates = simul_dates + hours(offset);
     simul_wl = Ttsimul.(stations{cwl});
     
     %constructing timetables
@@ -137,11 +142,20 @@ for cwl=1:length(stations)
         TT_mov_avg_main = synchronize(TT_mov_avg_main , TT_mov_avg);
     end
     
+    
+    %calculate different errors means
+    rmse = sqrt(sum((ttcomp_noNaN.simul_wl(:)-ttcomp_noNaN.meas_wl(:)).^2)/numel(ttcomp_noNaN.meas_wl));
+    me = sum((ttcomp_noNaN.simul_wl(:)-ttcomp_noNaN.meas_wl(:))/numel(ttcomp_noNaN.meas_wl));
+    mae = sum(abs(ttcomp_noNaN.simul_wl(:)-ttcomp_noNaN.meas_wl(:))/numel(ttcomp_noNaN.meas_wl));
+
     %subplots of water level comparison and difference
     h = figure('visible','off');
     
     ax1 = subplot(2,1,1);
     plot(ttmeas.meas_dates , ttmeas.meas_wl ,'-b');
+    xi = 0.85;
+    eta = 0.15;
+    text(xi,eta,sprintf('RMSE=%6.3f\n MAE=%6.3f\n  ME=%6.3f',rmse,mae,me),'Units','normalized','FontSize',6,'HorizontalAlignment','left');
     hold on
     plot(ttsimul.simul_dates , ttsimul.simul_wl ,'-r');
     hold on
@@ -152,14 +166,19 @@ for cwl=1:length(stations)
     ylabel('Water Level [m+NHN]/ Differences [m]');
     set(gca,'FontSize',6)
     ylim([-5 5])
+    yticks(-6:2:6);
     
     ax2 = subplot(2,1,2);
     plot(ttcomp_noNaN.meas_dates, wl_diff);
+    hold on
+    plot (ttcomp_noNaN.meas_dates, mov_avg);
     title(strcat('Water Level difference,', ' Station : ', stations(cwl)));
-    xlabel('Date/Time');
+    legend('Differences','Moving Average');
+    xlabel('Date/Time [UTC]');
     ylabel('Water Level Difference [m]');
     set(gca,'FontSize',6)
     ylim([-0.75 0.75])
+    yticks(-0.8:0.2:0.8);
     
     if ~isempty(ttcomp_noNaN.meas_dates)
         linkaxes([ax1 , ax2] , 'x');
@@ -195,9 +214,17 @@ for cwl=1:length(stations)
     clf
     close (h)
     
-    %normalized root mean square error
-    rmse = sqrt(sum((ttcomp_noNaN.simul_wl(:)-ttcomp_noNaN.meas_wl(:)).^2)/numel(ttcomp_noNaN.meas_wl));
-    mae = sum((ttcomp_noNaN.simul_wl(:)-ttcomp_noNaN.meas_wl(:))/numel(ttcomp_noNaN.meas_wl));
+    h = figure('visible','off'); 
+    scatter_kde(ttcomp_noNaN.simul_wl, ttcomp_noNaN.meas_wl, 'filled', 'MarkerSize', 5);
+    refline(1,0)
+    % Add Color bar
+    cb = colorbar();
+    cb.Label.String = 'Probability density estimate';
+    save_name_scatter = strcat(path_3, '/','WaterLevelScatter2', '_Station_', stations{cwl});
+    %    savefig(save_name_scatter);
+    saveas(gca, save_name_scatter , 'jpeg');
+    clf
+    close (h)
     
     if isnan(rmse)
         nrmse = NaN
@@ -206,80 +233,99 @@ for cwl=1:length(stations)
     end
     data_nrmse = vertcat(data_nrmse ,horzcat(stations(cwl), nrmse));
     data_rmse = vertcat(data_rmse ,horzcat(stations(cwl), rmse));
+    data_me = vertcat (data_me ,horzcat (stations(cwl), me));
     data_mae = vertcat (data_mae ,horzcat (stations(cwl), mae));
 end
 
 
 %% saving wl differences and wl differences moving averages for all stations
+if length(stations) > 0
+    TT_wl_diff_main.Properties.VariableNames = stations' ;
+    TT_mov_avg_main.Properties.VariableNames = stations' ;
+    
+    T_3 = timetable2table(TT_wl_diff_main);
+    saving_name = strcat(path_3 , '/', 'WL_Difference_all_stations' , '.dat');
+    writetable(T_3,char(saving_name));
+    T_4 = timetable2table(TT_mov_avg_main);
+    saving_name = strcat(path_3 , '/', 'WL_DifferenceMovingAvg_all_stations' , '.dat');
+    writetable(T_4,char(saving_name));
+    
+    %% plot nrmse for locations--------------------------------------------------
+    h = figure('visible','on');
+    plot(cell2mat(data_nrmse(:,2)),'rx')
+    set(gca, 'XTick', 1:length(data_nrmse))
+    set(gca,'XtickLabel',data_nrmse(:,1))
+    set(gca,'FontSize',6)
+    ylim([0 0.5])
+    title('NRMSE of WL on the required locations');
+    xlabel('Stations');
+    ylabel('NRMSE');
+    xtickangle(45);
+    save_name_NRMSE = strcat(path_3, '/','NRMSE of WL on the required locations');
+    % savefig(save_name_NRMSE);
+    saveas(gca, save_name_NRMSE , 'jpeg');
+    clf
+    close(h)
+    
+    % plot rmse for locations--------------------------------------------------
+    h = figure('visible','on');
+    ax1 = subplot(2,1,1);
+    plot(cell2mat(data_rmse(:,2)),'LineStyle','None','Marker','x','MarkerSize',6,'MarkerEdgeColor','r')
+    hold on
+    plot(cell2mat(data_mae(:,2)),'LineStyle','None','Marker','.','MarkerSize',10,'MarkerEdgeColor','k')
+    set(gca, 'XTick', 1:length(data_rmse))
+    set(gca,'XtickLabel',data_rmse(:,1))
+    set(gca,'FontSize',6)
+    xlim([0 length(data_rmse)]);
+    ylim([0 0.5])
+    legend('RMSE','MAE');
+    title('Root Mean Square and Mean Absolute Error of water level');
+    % xlabel('Stations');
+    ylabel('RMSE, MAE [m]');
+    xtickangle(45);
+    %save_name_RMSE = strcat(path_3, '/','RMSE of WL on the required locations');
+    % savefig(save_name_RMSE);
+    %saveas(gca, save_name_RMSE , 'jpeg');
+    
+    ax2 = subplot(2,1,2);
+    plot(cell2mat(data_me(:,2)),'r+')
+    set(gca, 'XTick', 1:length(data_me))
+    set(gca,'XtickLabel',data_me(:,1))
+    set(gca,'FontSize',6)
+    xlim([0 length(data_me)]);
+    ylim([-0.2 0.2])
+    title('Mean error of water level');
+    xlabel('Stations');
+    ylabel('ME [m]');
+    xtickangle(45);
 
-TT_wl_diff_main.Properties.VariableNames = stations' ;
-TT_mov_avg_main.Properties.VariableNames = stations' ;
+%     ax3 = subplot(3,1,3);
+%     plot(cell2mat(data_mae(:,2)),'rx')
+%     set(gca, 'XTick', 1:length(data_mae))
+%     set(gca,'XtickLabel',data_mae(:,1))
+%     set(gca,'FontSize',6)
+%     xlim([0 length(data_mae)]);
+%     ylim([0 0.4])
+%     title('MAE of water level [m]');
+%     xlabel('Stations');
+%     ylabel('MAE');
+%     xtickangle(45);
 
-T_3 = timetable2table(TT_wl_diff_main);
-saving_name = strcat(path_3 , '/', 'WL_Difference_all_stations' , '.dat');
-writetable(T_3,char(saving_name));
-T_4 = timetable2table(TT_mov_avg_main);
-saving_name = strcat(path_3 , '/', 'WL_DifferenceMovingAvg_all_stations' , '.dat');
-writetable(T_4,char(saving_name));
-
-%% plot nrmse for locations--------------------------------------------------
-h = figure('visible','on');
-plot(cell2mat(data_nrmse(:,2)),'rx')
-set(gca, 'XTick', 1:length(data_nrmse))
-set(gca,'XtickLabel',data_nrmse(:,1))
-set(gca,'FontSize',6)
-ylim([0 0.5])
-title('NRMSE of WL on the required locations');
-xlabel('Stations');
-ylabel('NRMSE');
-xtickangle(45);
-save_name_NRMSE = strcat(path_3, '/','NRMSE of WL on the required locations');
-% savefig(save_name_NRMSE);
-saveas(gca, save_name_NRMSE , 'jpeg');
-clf
-close(h)
-
-% plot rmse for locations--------------------------------------------------
-h = figure('visible','on');
-ax1 = subplot(2,1,1);
-plot(cell2mat(data_rmse(:,2)),'rx')
-set(gca, 'XTick', 1:length(data_rmse))
-set(gca,'XtickLabel',data_rmse(:,1))
-set(gca,'FontSize',6)
-ylim([0 0.5])
-title('RMSE of water level [m]');
-% xlabel('Stations');
-ylabel('RMSE');
-xtickangle(45);
-%save_name_RMSE = strcat(path_3, '/','RMSE of WL on the required locations');
-% savefig(save_name_RMSE);
-%saveas(gca, save_name_RMSE , 'jpeg');
-
-ax2 = subplot(2,1,2);
-plot(cell2mat(data_mae(:,2)),'rx')
-set(gca, 'XTick', 1:length(data_mae))
-set(gca,'XtickLabel',data_mae(:,1))
-set(gca,'FontSize',6)
-ylim([-0.2 0.2])
-title('MAE of water level [m]');
-xlabel('Stations');
-ylabel('MAE');
-xtickangle(45);
-
-grid(ax1,'on');
-grid(ax2,'on');
-ax1.FontSize = 6;
-ax2.FontSize = 6;
-pbaspect(ax1 , 'auto') %[x y z]
-pbaspect(ax2 , 'auto')
-
-save_name_RMSE = strcat(path_3, '/','RMSE of WL on the required locations');
-% savefig(save_name_RMSE);
-saveas(gca, save_name_RMSE , 'jpeg');
-clf
-close(h)
-
-close all
-
+    
+    grid(ax1,'on');
+    grid(ax2,'on');
+    ax1.FontSize = 6;
+    ax2.FontSize = 6;
+    pbaspect(ax1 , 'auto') %[x y z]
+    pbaspect(ax2 , 'auto')
+    
+    save_name_RMSE = strcat(path_3, '/','RMSE of WL on the required locations');
+    % savefig(save_name_RMSE);
+    saveas(gca, save_name_RMSE , 'jpeg');
+    clf
+    close(h)
+    
+    close all
+end
 end
 
